154f68a981701d94382902c5f761a2155712bc5e
Ahmed Hamdy <117328582+ahmedhamdyyounis@users.noreply.github.com>
2022-12-21 22:34:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 let xmark = document.getElementById("xmark")
 let bars = document.getElementById("bars")
 let mobMenu = document.getElementById("mob-menu-wrapper")
-console.log(mobMenu)
 
 bars.onclick = function () {
     xmark.style.display = "block"
@@ -17,6 +16,36 @@ xmark.onclick = function () {
     mobMenu.style.display = "none"
 }
 
-mobMenu.onclick = xmark.onclick
 
 //end toggle menu
+
+// pasta option
+
+let opt1 = document.getElementById("one")
+let opt2 = document.getElementById("two")
+
+let temp1 = document.getElementById("onee")
+let temp2 = document.getElementById("twoo")
+
+opt2.onclick = function () {
+        //active class green
+    opt1.classList.remove("act")
+    opt2.classList.add("act")
+//template selection
+    temp2.classList.add("d-block")
+    temp1.classList.add("d-none")
+    temp1.classList.remove("d-block")
+
+}
+
+opt1.onclick = function () {
+    //active class green
+    opt2.classList.remove("act")
+    this.classList.add("act")
+//template selection
+    temp2.classList.remove("d-block")
+    temp2.classList.add("d-none")
+    temp1.classList.add("d-block")
+}
+
+
