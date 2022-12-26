cc93fbe5118235429e9592066ec5ca56529bb85e
Ahmed Hamdy <117328582+ahmedhamdyyounis@users.noreply.github.com>
2022-12-27 00:24:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,13 @@
 let xmark = document.getElementById("xmark")
 let bars = document.getElementById("bars")
 let mobMenu = document.getElementById("mob-menu-wrapper")
+let menuCloseEmpty = document.getElementById("mob-close-empty")
+let angleDown = document.querySelectorAll(".fa-angle-down")
+let angleUp = document.querySelector(".fa-angle-up")
+let subCategorie = document.querySelector(".sub-categories")
+
+
+console.log(angleDown)
 
 bars.onclick = function () {
     xmark.style.display = "block"
@@ -14,6 +21,24 @@ xmark.onclick = function () {
     bars.style.display = "block"
     this.style.display = "none"
     mobMenu.style.display = "none"
+}
+
+menuCloseEmpty.onclick = function () {
+    mobMenu.style.display = "none"
+    bars.style.display = "block"
+}
+
+angleDown[1].onclick = function () {
+    subCategorie.classList.add("d-block")
+    this.classList.add("d-none")
+    angleUp.setAttribute("style", "display:block!important;")
+}
+angleUp.onclick = function () {
+    subCategorie.classList.add("d-none")
+    subCategorie.classList.remove("d-block")
+    angleUp.setAttribute("style", "display:none!important;")
+    angleDown[1].classList.remove("d-none")
+    
 }
 
 
@@ -35,8 +60,8 @@ opt2.onclick = function () {
     temp2.classList.add("d-block")
     temp1.classList.add("d-none")
     temp1.classList.remove("d-block")
-
 }
+
 
 opt1.onclick = function () {
     //active class green
@@ -53,9 +78,16 @@ opt1.onclick = function () {
 
 let close = document.getElementById("close")
 let popUp = document.getElementById("popup")
+let popUpWrapper = document.getElementById("popup-wrapper")
+console.log(popUpWrapper)
 
 
 close.onclick = function () {
+    popUp.classList.add("d-none")
+    
+}
+
+popUpWrapper.onclick = function () {
     popUp.classList.add("d-none")
 }
 
@@ -64,6 +96,9 @@ function popAppear() {
 }
 
 setTimeout(popAppear, 3000)
+
+
+
 
 
 
